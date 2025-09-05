7aedc5042e3ced244cbad9a9d63bfddd96b553f2
Shivendra <vshivendra1999@gmail.com>
2025-09-05 20:26:32 +0530

    Basic programmings like loops and operators

---
+++ Basics/even.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main() {
+
+int number  ;
+printf("enter number :");
+scanf("%d", &number);
+
+if (number % 2 == 0) {
+  printf("even number \n");   
+
+} else {
+    printf("odd number \n");
+}
+return 0;
+
+} 
