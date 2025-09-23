29fa43c2f649a6c7aa85d3ee4c9a7b77efb9b5ee
Shivendra <vshivendra1999@gmail.com>
2025-09-23 11:09:27 +0530

    Html

---
+++ Cpp/a.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+//This is my First program in cpp :)
+cout<<" 'Hello World' " ;
+return 0;
+}
--- Cpp/f.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>
-using namespace std;
-
-int main() {
-//This is my First program in cpp :)
-cout<<" 'Hello Shivendra Vishwakarma' " ;
-return 0;
-}
