e6231a6fcf75d8267d0680b868c105427f42a8c7
Shivendra <vshivendra1999@gmail.com>
2025-09-24 01:36:06 +0530

    Updating some basic things

---
+++ Cpp/a.cpp
@@ -0,0 +1,6 @@
+# include <iostream>
+using namespace std;
+int main() {
+    cout << "Hellooooooooooooo World!" << endl;
+    return 0;
+} 
---
+++ Cpp/hi.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;    
+
+int main() { 
+
+cout<< "Namaste Duniya"<<endl;
+cout<< "Hello World"<<endl;     
+
+return 0;    
+}
