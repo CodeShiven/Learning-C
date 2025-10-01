f9621884c7f3d9fbcbe8e16bdd95d7e9adf93957
Shivendra <vshivendra1999@gmail.com>
2025-10-01 17:06:34 +0530

    lil bit adjustments

--- Cpp/age.cpp
+++ Cpp/age.cpp
@@ -8,7 +8,7 @@ cout<<"Enter age:- ";
 cin>> age;
 
 if (age >= 18) {
-cout<<"You are adult and you can vote and drive and u can drink a little :)"<<endl;
+cout<<"You are an adult and you can vote and drive also u can drink a little :)"<<endl;
 }
 if (age < 18 && age > 12) {
 cout<<"You are not an adult so you cannot vote or drive also no drinking for u"<<endl;    
