6499f634c6daa1e3e1fbbfc77d0cf65d5b0173ab
Shivendra <vshivendra1999@gmail.com>
2026-01-13 01:45:34 +0530

    leaarning dsa

---
+++ Cpp/Loops/countdigitstilln.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+int n;
+cout<<"Enter n: ";
+cin>>n;
+int count=0;
+
+for (int i=1; i<=n; i++) {
+    count++;
+}
+cout<<count;
+
+
+return 0;    
+}
