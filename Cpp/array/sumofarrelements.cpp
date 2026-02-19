86b1b495ab7170b5191df6dc1380c19a1ec42675
Shivendra <vshivendra1999@gmail.com>
2026-02-19 23:43:25 +0530

    add program to calculate the sum of array elements

---
+++ Cpp/array/sumofarrelements.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+int size;
+cout<<"Enter size of array: ";
+cin>>size;
+
+int arr[size];
+for(int i=0; i<size; i++){
+    cin>>arr[i];
+}
+int sum = 0;
+for (int i=0; i<size; i++){
+    sum = sum + arr[i];
+}
+cout<<"Sum of Elements : "<<sum;
+
+
+return 0;    
+}
