7336606887c92b5eea9f4a2b25a504345ba0aca6
Shivendra <vshivendra1999@gmail.com>
2026-04-02 15:59:09 +0530

    revisiting programs for even/odd number check and multiplication table

---
+++ Cpp/Mixed practice questions/tableofnum.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main () {
+
+int num;
+cout<<"Enter a num: ";
+cin>>num;
+
+for (int i= 1; i<=104; i++){
+    cout<<num * i <<endl;
+}
+
+
+
+return 0;    
+}
