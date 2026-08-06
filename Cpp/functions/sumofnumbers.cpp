45e252879b01e51bb8720f23c60505998d233ecf
Shivendra <vshivendra1999@gmail.com>
2026-08-06 17:20:45 +0530

    add recursive function to calculate the sum of natural numbers

---
+++ Cpp/functions/sumofnumbers.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+
+int sumOfn(int n) {
+    if (n == 0){
+        return 0;
+    }
+    return n + sumOfn(n-1);
+    
+}
+
+
+int main() {
+
+cout<<sumOfn(5);
+
+    return 0;
+}
+
+
+
+
+
